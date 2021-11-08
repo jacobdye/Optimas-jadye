3b308e9f8d62a62476cd72abb48822e61024405a
Jacob Dye <jacobadye1@gmail.com>
2021-11-08 01:29:27 -0500

    Edit View class for numeric constraints

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -3,9 +3,20 @@ import java.util.Scanner;
 public class View {
     Scanner scanner = new Scanner(System.in);
     public int AskForInteger() {
-        System.out.println("What whole number integer would you like to convert to roman numerals?");
-        return Integer.parseInt(scanner.nextLine());
+        while(true) {
+            int input;
+            System.out.println("What whole number integer between 1-3000 " +
+                    "would you like to convert to roman numerals?");
+            input = Integer.parseInt(scanner.nextLine());
+            if (input >= 1 && input <= 3000) {
+                return input;
+            }
+            else {
+                System.out.println("Invalid input");
+            }
+        }
     }
+
     public void printRomanNumeral(String romanNumeral, int number) {
         System.out.println("The integer of " + number + " converts to roman numerals of: " );
         System.out.println(romanNumeral);
