1b60ceb017e97876d13d02775bddedebec17b7fa
Jacob Dye <jacobadye1@gmail.com>
2021-11-08 03:23:33 -0500

    Fix wrong math and indexing problems

--- src/main/java/Conversion.java
+++ src/main/java/Conversion.java
@@ -1,7 +1,7 @@
 public class Conversion {
     public StringBuilder ConvertIntToRoman(int input) {
         int[] arrayOfNumbers = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] arrayOfRomans = {"M", "CM", "D", "CD", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        String[] arrayOfRomans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         StringBuilder convertedRoman = new StringBuilder();
         for (int i = 0; i < arrayOfNumbers.length; i++) {
             while (input >= arrayOfNumbers[i]) {
