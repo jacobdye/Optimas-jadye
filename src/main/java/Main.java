f943ec5ad0ea474b31a13c23b47d2fe73bfaa783
Jacob Dye <jacobadye1@gmail.com>
2021-11-08 02:45:19 -0500

    Edit Main class to add method from Conversion class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         View view = new View();
+        Conversion conversion = new Conversion();
         int number;
         number = view.AskForInteger();
-        view.printRomanNumeral("", number);
+        StringBuilder romanNumeral = conversion.ConvertIntToRoman(number);
+        view.printRomanNumeral(romanNumeral, number);
     }
 }
